611b9aba9e3617a543b2268cdde5b7f993b3ac6c
alemuntoni <muntoni.alessandro@gmail.com>
2021-02-11 15:24:22 +0100

    added submodule OpenGR lib

---
+++ external/external.cmake
@@ -0,0 +1,6 @@
+# Copyright 2019, 2020, Collabora, Ltd.
+# Copyright 2019, 2020, Visual Computing Lab, ISTI - Italian National Research Council
+# SPDX-License-Identifier: BSL-1.0
+
+# opengr - optional and header-only, for filter_globalregistration
+include(${EXTERNAL_DIR}/opengr.cmake)
