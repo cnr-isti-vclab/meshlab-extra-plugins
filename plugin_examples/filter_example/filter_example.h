4750a639d2bab9faf00f49cc95bbd9dc49694293
alemuntoni <muntoni.alessandro@gmail.com>
2021-03-04 12:40:09 +0100

    codebase for plugin examples

---
+++ plugin_examples/filter_example/filter_example.h
@@ -0,0 +1,73 @@
+/****************************************************************************
+* MeshLab                                                           o o     *
+* A versatile mesh processing toolbox                             o     o   *
+*                                                                _   O  _   *
+* Copyright(C) 2005-2021                                           \/)\/    *
+* Visual Computing Lab                                            /\/|      *
+* ISTI - Italian National Research Council                           |      *
+*                                                                    \      *
+* All rights reserved.                                                      *
+*                                                                           *
+* This program is free software; you can redistribute it and/or modify      *
+* it under the terms of the GNU General Public License as published by      *
+* the Free Software Foundation; either version 2 of the License, or         *
+* (at your option) any later version.                                       *
+*                                                                           *
+* This program is distributed in the hope that it will be useful,           *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+* for more details.                                                         *
+*                                                                           *
+****************************************************************************/
+
+#ifndef MESHLAB_FILTER_EXAMPLE_PLUGIN_H
+#define MESHLAB_FILTER_EXAMPLE_PLUGIN_H
+
+#include <common/plugins/interfaces/filter_plugin.h>
+
+/**
+ * @brief The FilterExamplePlugin class
+ * This is a simple and useless example of a MeshLab filter plugin.
+ *
+ * You can use this class as a base to start and coding your plugin!
+ *
+ * You can change the name of the class. Just be sure that the class
+ * inherits from the QObject class (needed to make the plugin a Qt plugin)
+ * and the FilterPlugin class.
+ *
+ * The first three macros are mandatory in Filter plugins.
+ * Leave them there without modify them!
+ */
+class FilterExamplePlugin : public QObject, public FilterPlugin
+{
+	Q_OBJECT
+	MESHLAB_PLUGIN_IID_EXPORTER(FILTER_PLUGIN_IID)
+	Q_INTERFACES(FilterPlugin)
+
+public:
+	enum { FP_MOVE_VERTEX  } ;
+
+	FilterExamplePlugin();
+
+	QString pluginName() const;
+	QString vendor() const;
+
+	QString filterName(ActionIDType filter) const;
+	QString filterInfo(ActionIDType filter) const;
+	FilterClass getClass(const QAction* a) const;
+	FILTER_ARITY filterArity(const QAction*) const;
+	int getPreConditions(const QAction *) const;
+	int postCondition(const QAction* ) const;
+	void initParameterList(const QAction*, MeshModel &/*m*/, RichParameterList & /*parent*/);
+	bool applyFilter(const QAction* action, MeshDocument &md, std::map<std::string, QVariant>& outputValues, unsigned int& postConditionMask, const RichParameterList & /*parent*/, vcg::CallBackPos * cb);
+
+private:
+	bool vertexDisplacement(
+			MeshDocument &md,
+			vcg::CallBackPos *cb,
+			bool updateNormals,
+			Scalarm max_displacement);
+};
+
+#endif //MESHLAB_FILTER_EXAMPLE_PLUGIN_H
