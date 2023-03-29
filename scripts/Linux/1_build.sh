86c5ade10fdadc2d975387129da425a10ec39c17
Alessandro Muntoni <muntoni.alessandro@gmail.com>
2023-03-29 11:09:55 +0200

    fix install dir when build

--- scripts/Linux/1_build.sh
+++ scripts/Linux/1_build.sh
@@ -4,5 +4,6 @@
 SCRIPTS_PATH="$(dirname "$(realpath "$0")")"
 MESHLAB_SCRIPTS_PATH=$SCRIPTS_PATH/../../meshlab/scripts/Linux
 SOURCE_PATH=$SCRIPTS_PATH/../..
+INSTALL_PATH=$SOURCE_PATH/install/
 
-bash $MESHLAB_SCRIPTS_PATH/1_build.sh -s=$SOURCE_PATH $@
+bash $MESHLAB_SCRIPTS_PATH/1_build.sh -s=$SOURCE_PATH -i=$INSTALL_PATH $@ 
