0f882bc625af2e642ddd36993b4a5d143a92b039
Alessandro Muntoni <muntoni.alessandro@gmail.com>
2023-03-29 11:24:12 +0200

    fix install path on linux workflow

--- scripts/Linux/1_build.sh
+++ scripts/Linux/1_build.sh
@@ -8,4 +8,5 @@ INSTALL_PATH=$SOURCE_PATH/install/
 
 bash $MESHLAB_SCRIPTS_PATH/1_build.sh -s=$SOURCE_PATH -i=$INSTALL_PATH $@ 
 
+mkdir -p $INSTALL_PATH/meshlab/plugins/
 mv $INSTALL_PATH/usr/meshlab/plugins/* $INSTALL_PATH/meshlab/plugins/
